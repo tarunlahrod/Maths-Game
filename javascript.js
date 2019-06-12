d5da523b05c971856229d195724096182761446a
Tarun Lahrod <lahrod.tarun46@gmail.com>
2019-06-12 09:30:46 +0530

    added game logic

---
+++ javascript.js
@@ -0,0 +1,27 @@
+//--------X-------X-------GAME LOGIC--------X-------X--------//
+// If we click on the start/reset
+    // If we are playing
+        // reload page
+    // If we are not playing
+        // set score to 0
+        // show countdown timer
+        // reduce time by 1sec in loop
+            // if timeleft?
+                // yes - continue
+                // no - game over
+        // butoon to reset
+        // generate new Q&A
+
+
+// If we click on answer box
+    // If we are playing
+        // correct?
+            // yes
+                // Increase score
+                // show correct box for 1sec
+            // no
+                // show try again box
+
+//--------X-------X-------X--------X--------X-------X--------//
+
+document.getElementById("startreset");
