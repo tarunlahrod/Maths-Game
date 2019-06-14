24986a2c1a97a5d7406e113878ecbbce059a373b
Tarun Lahrod <lahrod.tarun46@gmail.com>
2019-06-14 23:36:11 +0530

    updated javascript

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+
+
 //--------X-------X-------GAME LOGIC--------X-------X--------//
 // If we click on the start/reset
     // If we are playing
@@ -24,4 +26,35 @@
 
 //--------X-------X-------X--------X--------X-------X--------//
 
-document.getElementById("startreset");
+var playing = false;
+var score;
+// If we click on the start/reset
+document.getElementById("startreset").onclick = function (){
+    // If we are playing
+    if (playing === true) {
+        location.reload();        // reload page
+    } else {    // If we are not playing
+        playing = true;
+        score = 0;        // set score to 0
+        document.getElementById("scorevalue").innerHTML = score;
+        document.getElementById("timeremaining").style.display = "block";        // show countdown timer
+        
+    }
+}
+        // reduce time by 1sec in loop
+            // if timeleft?
+                // yes - continue
+                // no - game over
+
+document.getElementById("startreset").innerHTML = "Reset Game";// butoon to reset
+        // generate new Q&A
+
+
+// If we click on answer box
+    // If we are playing
+        // correct?
+            // yes
+                // Increase score
+                // show correct box for 1sec
+            // no
+                // show try again box
